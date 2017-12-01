5e2467c04e317f74dbbbebfeb815e44392a24d8f
Alex Smith <alex_smith3@hotmail.com>
2017-12-01 14:06:50 +0000

    set up record and tests

---
+++ specs/record_spec.js
@@ -0,0 +1,19 @@
+var assert = require ("assert");
+var Record = require ("../record.js");
+
+describe("Record", function(){
+
+  var record;
+
+  beforeEach(function(){
+    record = new Record("Prince", "Purple Rain", "Funk", 10);
+  });
+
+  it("should have an artist");
+  it("should have a name");
+  it("should have a genre");
+  it("should have a price");
+
+
+
+})
