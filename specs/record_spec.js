48cc8886074f9a690e8508e68511b328196b9974
Alex Smith <alex_smith3@hotmail.com>
2017-12-01 14:08:15 +0000

    artist has name

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -9,7 +9,9 @@ describe("Record", function(){
     record = new Record("Prince", "Purple Rain", "Funk", 10);
   });
 
-  it("should have an artist");
+  it("should have an artist", function(){
+    assert.strictEqual(record.artist, "Prince");
+  });
   it("should have a name");
   it("should have a genre");
   it("should have a price");
