b11eeec8473f6eee3b4b5fafdc92f66947ca9391
Alex Smith <alex_smith3@hotmail.com>
2017-12-01 14:09:44 +0000

    record has title

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -12,7 +12,9 @@ describe("Record", function(){
   it("should have an artist", function(){
     assert.strictEqual(record.artist, "Prince");
   });
-  it("should have a name");
+  it("should have a title", function(){
+    assert.strictEqual(record.title, "Purple Rain");
+  });
   it("should have a genre");
   it("should have a price");
 
