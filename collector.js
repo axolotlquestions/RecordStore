0ae37d9c834b0341e80c2f02497ef2147f10de5d
Alex Smith <alex_smith3@hotmail.com>
2017-12-03 11:54:43 +0000

    buy function completed

--- collector.js
+++ collector.js
@@ -6,5 +6,18 @@ Collector = function(name){
   this.collection = [];
 };
 
+Collector.prototype = {
+  buy: function(record){
+    if(this.cash>record.price){
+      this.cash -= record.price;
+      this.collection.push(record)
+    }
+  },
+  sell: function(record){
+    this.cash += record.price;
+    _.pull(this.collection, record);
+  },
+
+};
 
 module.exports = Collector
--- specs/collector_spec.js
+++ specs/collector_spec.js
@@ -7,15 +7,38 @@ describe("Collector", function(){
   var collector1;
   var record1;
   var record2;
+  var record3
 
   beforeEach(function(){
     collector1 = new Collector("Alice");
     record1 = new Record("Prince", "Purple Rain", "Funk", 10);
     record2 = new Record("Led Zeppelin", "Led Zeppelin IV", "Rock", 20);
+    record3 = new Record("Parliament", "Mothership Connection", "Funk", 75);
+  });
+
+  it("should have cash" , function(){
+    assert.strictEqual(collector1.cash, 50);
+  });
+
+  it("should have cash that decreases with buying", function(){
+    collector1.buy(record1);
+    assert.strictEqual(collector1.collection.length, 1);
+    assert.strictEqual(collector1.cash, 40);
+  });
+
+  it("should have cash that increases with selling", function(){
+    collector1.buy(record1);
+    collector1.buy(record2);
+    collector1.sell(record2);
+    assert.strictEqual(collector1.collection.length, 1);
+    assert.strictEqual(collector1.cash, 40);
   })
 
-  it("should have cash that increase and decreases with buying and selling");
-  it("shouldn't be able to buy a Record if they can't afford it");
+  it("shouldn't be able to buy a Record if they can't afford it", function(){
+    collector1.buy(record3);
+    assert.strictEqual(collector1.collection.length, 0);
+  });
+
   it("should be able to view the total value of their collection");
   it("should be able to view the total value of all records of a given Genre");
   it("should be able to view their most valuable record");
